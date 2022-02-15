ceda4a748913a95252c8f77bc26687dc85a0b4e5
wiktordziadosz <szymonwik99gmail.com>
2022-02-15 16:24:10 +0100

    Vue template, Product and Categories models

--- GrailsPlug/Products/models.py
+++ GrailsPlug/Products/models.py
@@ -1,4 +1,78 @@
+from distutils.command.upload import upload
 from pyexpat import model
+#for handling images and resizings
+from io import BytesIO
+from statistics import mode
+from unicodedata import category
+from PIL import Image
+
+#fro thumbnails(miniaturki)
+from django.core.files import File
+
 from django.db import models
+
+class Categories(models.Model):
+    name = models.CharField(max_length=255)
+    #address version of name
+    slug = models.SlugField()
+
+    #model options
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/{self.slug}/'
+
+class Product(models.Model):
+    category = models.ForeignKey(Categories, related_name="products", on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    size = models.CharField(max_length=2)
+    brand = models.CharField(max_length=255)
+    image = models.ImageField(upload_to="uploads/", blank=True, null=True)
+    thumbnail = models.ImageField(upload_to="uploads/", blank=True, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+
+    class Meta:
+        # - stands for descending order
+        ordering = ('-date_added',)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/{self.category.slug}/{self.slug}/'
+    
+    def get_image(self):
+        if self.image:
+            return "http://127.0.0.1:8000" + self.image.url
+        return ""
+    
+    def get_thumbnail(self):
+        if self.thumbnail:
+            return "http://127.0.0.1:8000" + self.thumbnail.url
+        else:
+            if self.image:
+                self.thumbnail = self.make_thumbnail(self.image)
+                self.save()
+                return "http://127.0.0.1:8000" + self.thumbnail.url
+            else:
+                return ""
+    
+    def make_thumbnail(self, image, size):
+        size = (300, 200)
+        image = Image.open(image).convert('RGB').thumbnail(size)
+        thumb_io = BytesIO()
+        image.save(thumb_io, "JPEG", quality=85)
+        thumbnail = File(thumb_io, name=image.name)
+        return thumbnail
+
+
 
 
--- grailsplug_vue/src/App.vue
+++ grailsplug_vue/src/App.vue
@@ -1,30 +1,56 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="wrapper">
+      <nav class="navbar">
+        <div class="navbar-brand">
+            <router-link to="/" class="navbar-item" style="color:black;"><strong>GrailsPlug</strong></router-link>
+
+            <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu" @click="showMobilemMenu = !showMobilemMenu">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+            </a>
+        </div>
+        <div class="navbar-menu" id="navbar-menu" v-bind:class="{'is-active':showMobilemMenu }">
+            <div class="navbar-end">
+                <router-link to="/Summer" class="navbar-item" style="color:black;"><strong>Summer</strong></router-link>
+                <router-link to="/Winter" class="navbar-item" style="color:black;"><strong>Winter</strong></router-link>
+                <router-link to="/Man" class="navbar-item" style="color:black;"><strong>Man</strong></router-link>
+                <router-link to="/Woman" class="navbar-item" style="color:black;"><strong>Woman</strong></router-link>
+            </div>
+            <div class="navbar-item">
+                <div class="buttons">
+                    <router-link to="/LogIn" class="button is-light"><strong>Log in</strong></router-link>
+                    <router-link to="/Cart" class="button is-success">
+                        <span class="icon">
+                            <i class="fas fa-shopping-cart"></i>
+                        </span>
+                        <span><strong>Cart</strong></span>
+                    </router-link>
+                </div>
+            </div>
+        </div>
+      </nav> 
+        <section class="section">
+            <router-view/>
+        </section>
+
+        <footer>
+            <p class="has-text-centered">Copyright (C) 2021</p>
+        </footer>
   </div>
-  <router-view/>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+export default {
+    data() {
+        return {
+            showMobilemMenu: false,
+        }
     }
-  }
 }
+</script>
+
+<style lang="scss">
+@import '../node_modules/bulma';
 </style>
+
