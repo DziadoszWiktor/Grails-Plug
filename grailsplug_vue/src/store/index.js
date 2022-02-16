34aac28163723d59c903866b39b369d9085f4d90
wiktordziadosz <szymonwik99gmail.com>
2022-02-16 19:22:50 +0100

    Loading Bar

--- grailsplug_vue/src/store/index.js
+++ grailsplug_vue/src/store/index.js
@@ -34,10 +34,10 @@ export default createStore({
       }
 
       localStorage.setItem('cart', JSON.stringify(state.cart))
-    },/*
+    },
     setIsLoading(state, status) {
       state.isLoading = status
-    },
+    },/*
     setToken(state, token) {
         state.token = token
         state.isAuthenticated = true
